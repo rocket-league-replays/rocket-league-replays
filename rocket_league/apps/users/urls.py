ec94933bf01ef01856c2fbe6fb32c295f9f5c039
Daniel Samuels <daniel.samuels1@gmail.com>
2015-08-09 20:21:33 +0100

    Add public user profiles.

--- rocket_league/apps/users/urls.py
+++ rocket_league/apps/users/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
 
-from .views import ProfileView
+import views
 
 urlpatterns = patterns(
     '',
-    url(r'^profile/$', ProfileView.as_view(), name='profile'),
+    url(r'^profile/$', views.ProfileView.as_view(), name='profile'),
+    url(r'^user/(?P<username>[^/]+)/$', views.PublicProfileView.as_view(), name='profile'),
 )
--- rocket_league/apps/users/views.py
+++ rocket_league/apps/users/views.py
@@ -1,7 +1,15 @@
-from django.views.generic import TemplateView
+from django.contrib.auth.models import User
+from django.views.generic import DetailView, TemplateView
 
 from braces.views import LoginRequiredMixin
 
 
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'users/user_profile.html'
+
+
+class PublicProfileView(DetailView):
+    model = User
+    slug_url_kwarg = 'username'
+    slug_field = 'username'
+    template_name = 'users/user_public_profile.html'
