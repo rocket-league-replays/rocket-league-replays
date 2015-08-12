cc5ed6992d4cec5eb6d300f33a69252983982d4d
Daniel Samuels <daniel.samuels1@gmail.com>
2015-08-12 23:37:05 +0100

    Add user settings form.

---
+++ rocket_league/apps/users/forms.py
@@ -0,0 +1,88 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class UserSettingsForm(forms.ModelForm):
+
+    error_messages = {
+        'password_mismatch': "The two password fields didn't match.",
+        'password_incorrect': "Your current password was entered incorrectly. Please enter it again.",
+    }
+
+    old_password = forms.CharField(
+        label="Current password",
+        widget=forms.PasswordInput,
+        required=False,
+    )
+
+    new_password1 = forms.CharField(
+        label="New password",
+        widget=forms.PasswordInput,
+        required=False,
+    )
+
+    new_password2 = forms.CharField(
+        label="New password confirmation",
+        widget=forms.PasswordInput,
+        required=False,
+    )
+
+    def clean(self):
+        print self.instance
+        """
+        Validates that the old_password field is correct.
+        """
+        cleaned_data = super(UserSettingsForm, self).clean()
+
+        old_password = cleaned_data.get('old_password')
+        password1 = cleaned_data.get('new_password1')
+        password2 = cleaned_data.get('new_password2')
+
+        # If we don't have the old password, but we do have a new password
+        # then we have an error.
+        if not old_password and (password1 or password2):
+            self._errors['old_password'] = self.error_class([
+                'To change your password you must enter your current password.'
+            ])
+
+        if old_password:
+            if not self.instance.check_password(old_password):
+                self._errors['old_password'] = self.error_class([
+                    self.error_messages['password_incorrect'],
+                ])
+
+        password_errors = []
+
+        if password1 and password2 and old_password:
+            if password1 != password2:
+                password_errors.append("The two password fields didn't match.")
+
+        if password_errors:
+            self._errors['new_password1'] = password_errors
+            self._errors['new_password2'] = password_errors
+
+        return cleaned_data
+
+    def clean_new_password2(self):
+        password1 = self.cleaned_data.get('new_password1')
+        password2 = self.cleaned_data.get('new_password2')
+        if password1 and password2:
+            if password1 != password2:
+                raise forms.ValidationError(
+                    self.error_messages['password_mismatch'],
+                    code='password_mismatch',
+                )
+        return password2
+
+    def save(self, commit=True):
+        if self.cleaned_data['new_password1']:
+            self.instance.set_password(self.cleaned_data['new_password1'])
+
+        if commit:
+            self.instance.save()
+
+        return self.instance
+
+    class Meta:
+        model = User
+        fields = ['username', 'old_password', 'new_password1', 'new_password2']
--- rocket_league/apps/users/views.py
+++ rocket_league/apps/users/views.py
@@ -1,11 +1,26 @@
 from django.contrib.auth.models import User
-from django.views.generic import DetailView, TemplateView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.core.urlresolvers import reverse
+from django.views.generic import DetailView, UpdateView
+
+from .forms import UserSettingsForm
 
 from braces.views import LoginRequiredMixin
 
 
-class ProfileView(LoginRequiredMixin, TemplateView):
+class ProfileView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     template_name = 'users/user_profile.html'
+    model = User
+    form_class = UserSettingsForm
+    success_message = "Your settings were successfully updated."
+
+    def get_object(self):
+        return self.request.user
+
+    def get_success_url(self):
+        return '{}#settings-tab'.format(
+            reverse('users:profile')
+        )
 
 
 class PublicProfileView(DetailView):
