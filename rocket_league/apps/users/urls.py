8f70518bdf466438101c4312f589dfe1a8e34053
Daniel Samuels <daniel.samuels1@gmail.com>
2015-10-05 20:20:03 +0100

    Split user profile pages into their own templates.

--- rocket_league/apps/users/urls.py
+++ rocket_league/apps/users/urls.py
@@ -4,6 +4,10 @@ import views
 
 urlpatterns = patterns(
     '',
-    url(r'^profile/$', views.ProfileView.as_view(), name='profile'),
     url(r'^user/(?P<username>[^/]+)/$', views.PublicProfileView.as_view(), name='profile'),
+    url(r'^profile/$', views.UserReplaysView.as_view(), name='profile'),
+    url(r'^profile/replay-packs/$', views.UserReplayPacksView.as_view(), name='replay_packs'),
+    url(r'^profile/desktop-application/$', views.UserDesktopApplicationView.as_view(), name='desktop_application'),
+    url(r'^profile/rank-tracker/$', views.UserRankTrackerView.as_view(), name='rank_tracker'),
+    url(r'^profile/settings/$', views.UserSettingsView.as_view(), name='settings'),
 )
