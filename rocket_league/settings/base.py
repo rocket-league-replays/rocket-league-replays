46103904d2506e41035ac8a022a23c901b913851
Daniel Samuels <daniel.samuels1@gmail.com>
2015-10-06 20:44:55 +0100

    Enable token authentication as an API auth method.

--- rocket_league/settings/base.py
+++ rocket_league/settings/base.py
@@ -287,7 +287,9 @@ REDACTOR_OPTIONS = {
 }
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
