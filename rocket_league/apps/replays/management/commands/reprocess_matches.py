350ca09176cb287aa4a942ddac4d7116cf26cf73
Daniel Samuels <daniel.samuels1@gmail.com>
2015-10-09 09:35:21 +0100

    Allow `reprocess_matches` to be run on singular replays.

--- rocket_league/apps/replays/management/commands/reprocess_matches.py
+++ rocket_league/apps/replays/management/commands/reprocess_matches.py
@@ -6,8 +6,15 @@ from ...models import Replay
 class Command(BaseCommand):
     help = "Re-process all replays."
 
+    def add_arguments(self, parser):
+        # Positional arguments
+        parser.add_argument('replay_id', nargs='?', type=int)
+
     def handle(self, *args, **options):
-        replays = Replay.objects.all()
+        if options['replay_id']:
+            replays = Replay.objects.filter(pk=options['replay_id'])
+        else:
+            replays = Replay.objects.all()
 
         for replay in replays:
             if replay.replay_id and replay.file:
