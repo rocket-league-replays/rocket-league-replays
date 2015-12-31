1d812ce594d75f746329fd641d9761f604b54da7
Dan Gamble <dangamble89@gmail.com>
2015-12-31 12:13:13 +0000

    Added new needed files for frontend flow

---
+++ gulpfile.js/tasks/css.js
@@ -0,0 +1,34 @@
+var config       = require('../config')
+if(!config.tasks.css) return
+
+var gulp         = require('gulp')
+var browserSync  = require('browser-sync')
+var sourcemaps   = require('gulp-sourcemaps')
+var postcss      = require('gulp-postcss')
+var handleErrors = require('../lib/handleErrors')
+var path         = require('path')
+
+var paths = {
+  src: path.join(config.root.src, config.tasks.css.src, '/*.' + config.tasks.css.extensions),
+  dest: path.join(config.root.dest, config.tasks.css.dest)
+}
+
+var cssTask = function () {
+  return gulp.src(paths.src)
+    .pipe(sourcemaps.init())
+    .pipe(postcss(require('../lib/postCssProcessors')))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.dest))
+    .pipe(browserSync.stream({match: '**/*.css'}))
+}
+
+var cssProductionTask = function () {
+  return gulp.src(path.join(config.root.dest, config.tasks.css.dest, '/*.' + config.tasks.css.extensions))
+    .pipe(postcss([require('cssnano')]))
+    .pipe(gulp.dest(paths.dest))
+    .pipe(browserSync.stream({match: '**/*.css'}))
+}
+
+gulp.task('css', ['stylelint'], cssTask)
+gulp.task('css:production', ['css', 'stylelint'], cssProductionTask)
+module.exports = cssTask
---
+++ gulpfile.js/tasks/stylelint.js
@@ -0,0 +1,25 @@
+var config = require('../config')
+if (!config.tasks.css) return
+
+var gulp = require('gulp')
+var postcss = require('gulp-postcss')
+var path = require('path')
+var reporter = require('postcss-reporter')
+
+var paths = {
+  src: path.join(config.root.src, config.tasks.css.src, '/**/*.' + config.tasks.css.extensions),
+  dest: path.join(config.root.dest, config.tasks.css.dest)
+}
+
+var styleLintTask = function () {
+  var processors = [
+    require('stylelint'),
+    reporter({clearMessages: true})
+  ]
+
+  return gulp.src(paths.src)
+    .pipe(postcss(processors))
+}
+
+gulp.task('stylelint', styleLintTask)
+module.exports = styleLintTask
