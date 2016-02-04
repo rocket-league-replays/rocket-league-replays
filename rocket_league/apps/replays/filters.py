31a79349a32a8d0f737ddc52c30e1e245723b835
Daniel Samuels <daniel.samuels1@gmail.com>
2016-02-04 22:11:23 +0000

    Re-work the replay listing to show season, removing excitement factor. Slim down filters.

--- rocket_league/apps/replays/filters.py
+++ rocket_league/apps/replays/filters.py
@@ -15,10 +15,6 @@ class ReplayFilter(django_filters.FilterSet):
         lookup_type='icontains',
     )
 
-    server_name = django_filters.filters.CharFilter(
-        lookup_type='icontains',
-    )
-
     team_sizes = django_filters.filters.ChoiceFilter(
         choices=(
             (None, 'Any'),
@@ -27,16 +23,6 @@ class ReplayFilter(django_filters.FilterSet):
             (3, '3v3'),
             (4, '4v4'),
         )
-    )
-
-    excitement_factor = django_filters.filters.NumberFilter(
-        lookup_type='gte',
-    )
-
-    total_goals = django_filters.filters.NumberFilter(
-        action=lambda qs, value: qs.annotate(
-            goals=Sum(F('team_0_score') + F('team_1_score'))
-        ).filter(goals=value)
     )
 
     region = django_filters.filters.ChoiceFilter(
@@ -52,23 +38,11 @@ class ReplayFilter(django_filters.FilterSet):
         lookup_type='startswith',
     )
 
-    match_length = django_filters.filters.NumberFilter(
-        label='Match length (in seconds)',
-        action=lambda qs, value: qs.annotate(
-            seconds=ExpressionWrapper(
-                F('num_frames') / F('record_fps'),
-                output_field=models.IntegerField()
-            )
-        ).filter(
-            seconds__gte=value,
-        )
-    )
-
     order_by_field = 'order'
 
     class Meta:
         model = Replay
-        fields = ['map', 'server_name', 'team_sizes', 'excitement_factor']
+        fields = ['map', 'team_sizes', 'season']
         strict = False
         # order_by = ['-average_rating', '-excitement_factor']
 
