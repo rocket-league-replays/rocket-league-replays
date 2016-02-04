b6c2a2ff374d19592a8bb487f957a5290524507c
Daniel Samuels <daniel.samuels1@gmail.com>
2016-02-04 19:20:49 +0000

    Fix Webpack file naming scheme.

--- gulpfile.js/lib/webpack-multi-config.js
+++ gulpfile.js/lib/webpack-multi-config.js
@@ -11,7 +11,7 @@ module.exports = function(env) {
   var jsSrc = path.resolve(config.root.src, config.tasks.js.src)
   var jsDest = path.resolve(config.root.dest, config.tasks.js.dest)
   var publicPath = path.join('/static/build/', config.tasks.js.dest, '/')
-  var filenamePattern = env === 'production' ? '[name]-[hash].js' : '[name].js'
+  var filenamePattern = '[name].js'
   var extensions = config.tasks.js.extensions.map(function(extension) {
     return '.' + extension
   })
