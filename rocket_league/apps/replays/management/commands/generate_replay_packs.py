ff099fb6cb0a74707ee1438c3b790ce7d1644184
Daniel Samuels <daniel.samuels1@gmail.com>
2016-02-26 21:02:19 +0000

    Move replay pack generation to a cron job.

---
+++ rocket_league/apps/replays/management/commands/generate_replay_packs.py
@@ -0,0 +1,51 @@
+from django.core.management.base import BaseCommand
+import requests
+from django.template.loader import render_to_string
+from django.core.files.base import ContentFile
+import StringIO
+from zipfile import ZipFile
+from ...models import ReplayPack
+
+
+class Command(BaseCommand):
+    help = "Generate ZIP files for replay packs without files"
+
+    def add_arguments(self, parser):
+        # Positional arguments
+        parser.add_argument('replay_pack_id', nargs='?', type=int)
+
+    def handle(self, *args, **options):
+        if options['replay_pack_id']:
+            replay_packs = ReplayPack.objects.filter(pk=options['replay_pack_id'])
+        else:
+            replay_packs = ReplayPack.objects.filter(
+                file='',
+            )
+
+        replay_packs = replay_packs.order_by('pk')
+
+        for obj in replay_packs:
+            print 'Processing', obj.pk
+            zip_filename = '{}.zip'.format(str(obj))
+
+            zip_string = StringIO.StringIO()
+
+            with ZipFile(zip_string, 'w') as f:
+                for replay in obj.replays.all():
+                    filename = '{}.replay'.format(replay.replay_id)
+
+                    print 'Getting {}'.format(replay.file.url)
+                    r = requests.get(replay.file.url, stream=True)
+
+                    for chunk in r.iter_content(1024):
+                        f.writestr(filename, chunk)
+
+                # Create a README file.
+                readme = render_to_string('replays/readme.html', {
+                    'replaypack': obj,
+                })
+
+                f.writestr('README.txt', str(readme))
+            f.close()
+
+            obj.file.save(zip_filename, ContentFile(zip_string.getvalue()))
