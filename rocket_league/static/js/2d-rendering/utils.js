a4e39f53ff433fb0ffc766becc305eaae5b7c737
Daniel Samuels <daniel.samuels1@gmail.com>
2016-04-02 14:12:06 +0100

    Add semi-colons to utils.js

--- rocket_league/static/js/2d-rendering/utils.js
+++ rocket_league/static/js/2d-rendering/utils.js
@@ -30,10 +30,10 @@ function formatTime (time) {
   let ret = ''
 
   if (hrs > 0) {
-    ret += `${hrs}:${(mins < 10 ? '0' : '')}`
+    ret += `${hrs}:${(mins < 10 ? '0' : '')}`;
   }
 
-  ret += `${mins}:${(secs < 10 ? '0' : '')}`
-  ret += `${secs.toFixed(0)}`
+  ret += `${mins}:${(secs < 10 ? '0' : '')}`;
+  ret += `${secs.toFixed(0)}`;
   return ret
 }
