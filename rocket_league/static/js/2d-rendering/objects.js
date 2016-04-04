94f45637b6c5a10fc58690f4d883c1c23a02c1ae
Daniel Samuels <daniel.samuels1@gmail.com>
2016-04-04 20:00:33 +0100

    Change 2d rendering JS files to ES5.

--- rocket_league/static/js/2d-rendering/objects.js
+++ rocket_league/static/js/2d-rendering/objects.js
@@ -1,158 +1,149 @@
 /*global THREE, textureLoader, scene, r, teamData, actorData, carsLoading:true*/
-'use strict'
+'use strict';
 
-function renderRect (width, height) {
-  const rectShape = new THREE.Shape()
-  rectShape.moveTo(-(width / 2), -(height / 2))
-  rectShape.lineTo(width / 2, -(height / 2))
-  rectShape.lineTo(width / 2, height / 2)
-  rectShape.lineTo(-(width / 2), height / 2)
-  rectShape.lineTo(-(width / 2), -(height / 2))
+function renderRect(width, height) {
+  var rectShape = new THREE.Shape();
+  rectShape.moveTo(-(width / 2), -(height / 2));
+  rectShape.lineTo(width / 2, -(height / 2));
+  rectShape.lineTo(width / 2, height / 2);
+  rectShape.lineTo(-(width / 2), height / 2);
+  rectShape.lineTo(-(width / 2), -(height / 2));
 
-  return new THREE.ShapeGeometry(rectShape)
+  return new THREE.ShapeGeometry(rectShape);
 }
 
 // Load the stadium outline.
-function addStadium () {
-  const materials = [
-    new THREE.MeshLambertMaterial({
-      map: textureLoader.load(resource__arena_fieldlines),
-      transparent: true,
-      opacity: 0.5,
-      color: 0xffffff
-    }),
-    new THREE.MeshLambertMaterial({
-      map: textureLoader.load(resource__arena_overlay),
-      transparent: true,
-      opacity: 0.7,
-      color: 0xffffff
-    }),
-    new THREE.MeshLambertMaterial({
-      map: textureLoader.load(resource__arena_boost),
-      transparent: true,
-      color: 0xffffff,
-      opacity: 0.5
-    })
-  ]
+function addStadium() {
+  var materials = [new THREE.MeshLambertMaterial({
+    map: textureLoader.load(resource__arena_fieldlines),
+    transparent: true,
+    opacity: 0.5,
+    color: 0xffffff
+  }), new THREE.MeshLambertMaterial({
+    map: textureLoader.load(resource__arena_overlay),
+    transparent: true,
+    opacity: 0.7,
+    color: 0xffffff
+  }), new THREE.MeshLambertMaterial({
+    map: textureLoader.load(resource__arena_boost),
+    transparent: true,
+    color: 0xffffff,
+    opacity: 0.5
+  })];
 
-  const mesh = new THREE.SceneUtils.createMultiMaterialObject(
-    new THREE.PlaneGeometry(8240, 12000),
-    materials
-  )
+  var mesh = new THREE.SceneUtils.createMultiMaterialObject(new THREE.PlaneGeometry(8240, 12000), materials);
 
-  mesh.name = 'stadium'
-  mesh.castShadow = false
-  mesh.receiveShadow = false
+  mesh.name = 'stadium';
+  mesh.castShadow = false;
+  mesh.receiveShadow = false;
 
-  scene.add(mesh)
+  scene.add(mesh);
 }
 
-function addCar (name, actor) {
-  console.log(`[${name}] Adding car`)
-  let texture
+function addCar(name, actor) {
+  console.log('[' + name + '] Adding car');
+  var texture = void 0;
 
   if (actor.y < 0) {
-    texture = textureLoader.load(resource__Car_Body_0)
+    texture = textureLoader.load(resource__Car_Body_0);
   } else {
-    texture = textureLoader.load(resource__Car_Body_1)
+    texture = textureLoader.load(resource__Car_Body_1);
   }
 
-  const mesh = new THREE.Mesh(
-    // renderRect(74, 144),
-    new THREE.BoxGeometry(90, 175, 1),
-    new THREE.MeshLambertMaterial({
-      map: texture,
-      transparent: true,
-      opacity: 1
-    })
-  )
+  var mesh = new THREE.Mesh(
+  // renderRect(74, 144),
+  new THREE.BoxGeometry(90, 175, 1), new THREE.MeshLambertMaterial({
+    map: texture,
+    transparent: true,
+    opacity: 1
+  }));
 
-  mesh.name = name
-  mesh.matrixAutoUpdate = true
-  mesh.castShadow = true
-  mesh.receiveShadow = true
+  mesh.name = name;
+  mesh.matrixAutoUpdate = true;
+  mesh.castShadow = true;
+  mesh.receiveShadow = true;
 
-  console.log(`[${name}] Adding to scene`)
+  console.log('[' + name + '] Adding to scene');
 
   // Fix the rotation point.
-  const box = new THREE.Box3().setFromObject(mesh)
-  box.center(mesh.position) // this re-sets the mesh position
-  mesh.position.multiplyScalar(-1)
+  var box = new THREE.Box3().setFromObject(mesh);
+  box.center(mesh.position); // this re-sets the mesh position
+  mesh.position.multiplyScalar(-1);
 
-  scene.add(mesh)
+  scene.add(mesh);
 
   // Reposition the car based on the latest data.
-  console.log(`[${name}] Setting initial position`)
+  console.log('[' + name + '] Setting initial position');
 
-  mesh.position.set(actor.x * -1, actor.y, actor.z)
-  mesh.rotation.set(0, 0, r(90) + actor.pitch * Math.PI * -1)
+  mesh.position.set(actor.x * -1, actor.y, actor.z);
+  mesh.rotation.set(0, 0, r(90) + actor.pitch * Math.PI * -1);
 
   // Add the boost gauge.
   /*
   <div class="boost-outer boost-player-{{ player.actor_id }}">
       {{ player.player_name }}
-
-      <div class="boost-inner" data-team="{{ player.team }}">34</div>
+       <div class="boost-inner" data-team="{{ player.team }}">34</div>
   </div>
   */
 
   if (actorData[actor.id] !== undefined) {
-    const team = teamData[actorData[actor.id].team]
+    var team = teamData[actorData[actor.id].team];
 
-    const outerElement = document.createElement('div')
-    outerElement.classList.add('sim-Boost_Outer')
-    outerElement.classList.add(`sim-Boost_Outer-actor${actor.id}`)
+    var outerElement = document.createElement('div');
+    outerElement.classList.add('sim-Boost_Outer');
+    outerElement.classList.add('sim-Boost_Outer-actor' + actor.id);
 
-    const playerName = document.createElement('span')
-    playerName.innerHTML = actorData[actor.id].name
-    playerName.classList.add('sim-Boost_Text')
-    outerElement.appendChild(playerName)
+    var playerName = document.createElement('span');
+    playerName.innerHTML = actorData[actor.id].name;
+    playerName.classList.add('sim-Boost_Text');
+    outerElement.appendChild(playerName);
 
-    const innerElement = document.createElement('div')
-    innerElement.innerHTML = '34'
-    innerElement.classList.add('sim-Boost_Inner')
-    innerElement.classList.add(`sim-Boost_Inner-team${team}`)
-    innerElement.classList.add(`sim-Boost_Inner-player${actor.id}`)
-    outerElement.appendChild(innerElement)
+    var innerElement = document.createElement('div');
+    innerElement.innerHTML = '34';
+    innerElement.classList.add('sim-Boost_Inner');
+    innerElement.classList.add('sim-Boost_Inner-team' + team);
+    innerElement.classList.add('sim-Boost_Inner-player' + actor.id);
+    outerElement.appendChild(innerElement);
 
-    document.querySelector(`.sim-Boost-team${team}`).appendChild(outerElement)
+    document.querySelector('.sim-Boost-team' + team).appendChild(outerElement);
   }
 
-  console.log(`[${name}] Render complete`)
-  carsLoading = carsLoading.filter(function (e) { return e !== name })
+  console.log('[' + name + '] Render complete');
+  carsLoading = carsLoading.filter(function (e) {
+    return e !== name;
+  });
 }
 
-function addBall (name, actor) {
-  console.log(`[${name}] Adding ball`)
+function addBall(name, actor) {
+  console.log('[' + name + '] Adding ball');
 
-  const mesh = new THREE.Mesh(
-    new THREE.CircleGeometry(91, 32),
-    new THREE.MeshLambertMaterial({
-      map: textureLoader.load(resource__ball)
-    })
-  )
+  var mesh = new THREE.Mesh(new THREE.CircleGeometry(91, 32), new THREE.MeshLambertMaterial({
+    map: textureLoader.load(resource__ball)
+  }));
 
-  mesh.name = name
-  mesh.matrixAutoUpdate = true
-  mesh.castShadow = true
-  mesh.receiveShadow = true
-  mesh.renderOrder = 1
+  mesh.name = name;
+  mesh.matrixAutoUpdate = true;
+  mesh.castShadow = true;
+  mesh.receiveShadow = true;
+  mesh.renderOrder = 1;
 
-  console.log(`[${name}] Adding to scene`)
+  console.log('[' + name + '] Adding to scene');
 
   // Fix the rotation point.
-  const box = new THREE.Box3().setFromObject(mesh)
-  box.center(mesh.position) // this re-sets the mesh position
-  mesh.position.multiplyScalar(-1)
+  var box = new THREE.Box3().setFromObject(mesh);
+  box.center(mesh.position); // this re-sets the mesh position
+  mesh.position.multiplyScalar(-1);
 
-  scene.add(mesh)
+  scene.add(mesh);
 
   // Reposition the car based on the latest data.
-  console.log(`[${name}] Setting initial position`)
+  console.log('[' + name + '] Setting initial position');
 
-  mesh.position.set(actor.x * -1, actor.y, actor.z)
-  mesh.rotation.set(0, 0, actor.yaw * Math.PI)
+  mesh.position.set(actor.x * -1, actor.y, actor.z);
+  mesh.rotation.set(0, 0, actor.yaw * Math.PI);
 
-  console.log(`[${name}] Render complete`)
-  carsLoading = carsLoading.filter(function (e) { return e !== name })
+  console.log('[' + name + '] Render complete');
+  carsLoading = carsLoading.filter(function (e) {
+    return e !== name;
+  });
 }
--- rocket_league/static/js/2d-rendering/replay.js
+++ rocket_league/static/js/2d-rendering/replay.js
@@ -1,104 +1,104 @@
 /*global init, formatTime, scene, addCar, addBall, r*/
-'use strict'
+'use strict';
 
-let currentFrame = -1
-let maxFrame = -1
-let frameData = {}
-let goalData = []
-let boostData = {}
-let secondsData = {}
-let carsLoading = []
-let actorData = {}
-let teamData = {}
+var currentFrame = -1;
+var maxFrame = -1;
+var frameData = {};
+var goalData = [];
+var boostData = {};
+var secondsData = {};
+var carsLoading = [];
+var actorData = {};
+var teamData = {};
 
-function loadGameData (url) {
-  init()
+function loadGameData(url) {
+  init();
 
-  const request = new XMLHttpRequest()
+  var request = new XMLHttpRequest();
 
-  request.open('GET', url, true)
+  request.open('GET', url, true);
 
   request.onload = function () {
     if (request.status >= 200 && request.status < 400) {
-      const response_data = JSON.parse(request.responseText)
+      var response_data = JSON.parse(request.responseText);
 
       if (response_data.frame_data !== undefined) {
-        frameData = response_data.frame_data
+        frameData = response_data.frame_data;
       }
 
       if (response_data.goals !== undefined) {
-        goalData = response_data.goals
+        goalData = response_data.goals;
       }
 
       if (response_data.boost !== undefined) {
-        boostData = response_data.boost
+        boostData = response_data.boost;
       }
 
       if (response_data.seconds_mapping !== undefined) {
-        secondsData = response_data.seconds_mapping
+        secondsData = response_data.seconds_mapping;
       }
 
       if (response_data.actors !== undefined) {
-        actorData = response_data.actors
+        actorData = response_data.actors;
       }
 
       if (response_data.teams !== undefined) {
-        teamData = response_data.teams
+        teamData = response_data.teams;
       }
 
-      maxFrame = Object.keys(frameData).length
-      currentFrame = 0
+      maxFrame = Object.keys(frameData).length;
+      currentFrame = 0;
     }
-  }
+  };
 
-  request.send()
+  request.send();
 }
 
-function positionReplayObjects () {
+function positionReplayObjects() {
   if (carsLoading.length > 0) {
-    console.warn(`Still rendering ${carsLoading}`)
+    console.warn('Still rendering ' + carsLoading);
   }
 
   if (secondsData[currentFrame] !== undefined) {
-    document.querySelector('.sim-Timer_Value').innerHTML = formatTime(secondsData[currentFrame])
+    document.querySelector('.sim-Timer_Value').innerHTML = formatTime(secondsData[currentFrame]);
   }
 
   // Is there any boost data for this frame?
   Object.keys(boostData.values).forEach(function (item) {
     if (boostData.values[item][currentFrame] !== undefined) {
       // Which player is this?
-      const player_id = boostData.cars[boostData.actors[item]]
+      var player_id = boostData.cars[boostData.actors[item]];
 
       // Current in-game (and %) value.
-      const value = Math.ceil(boostData.values[item][currentFrame] * (100 / 255))
+      var value = Math.ceil(boostData.values[item][currentFrame] * (100 / 255));
 
-      const boostEl = document.querySelector(`.sim-Boost_Inner-player${player_id}`)
+      var boostEl = document.querySelector('.sim-Boost_Inner-player' + player_id);
 
       if (boostEl) {
-        boostEl.style.backgroundSize = `${value}% 100%`;
-        boostEl.innerHTML = value
+        boostEl.style.backgroundSize = value + '% 100%';
+        boostEl.innerHTML = value;
 
         if (value < 15) {
-          boostEl.style.color = '#000'
+          boostEl.style.color = '#000';
         } else {
-          boostEl.style.color = ''
+          boostEl.style.color = '';
         }
       }
     } else {
       // Search for boost values within the next 74 frames.
-      let searchLength = 1
+      var searchLength = 1;
 
-      for (let i=currentFrame; i<currentFrame+74; i++) {
+      for (var i = currentFrame; i < currentFrame + 74; i++) {
         if (boostData.values[item][i] !== undefined) {
           // Which player is this?
-          const player_id = boostData.cars[boostData.actors[item]]
-          const boostEl = document.querySelector(`.sim-Boost_Inner-player${player_id}`)
+          var _player_id = boostData.cars[boostData.actors[item]];
+          var _boostEl = document.querySelector('.sim-Boost_Inner-player' + _player_id);
 
-          if (boostEl) {
-            const frameDiff = i - currentFrame
+          if (_boostEl) {
+            var frameDiff = i - currentFrame;
             // Boost decreases at a rate of ~255/74 per frame.
-            const currentValue = boostEl.innerText / (100 / 255)
-            const nextValue = boostData.values[item][i]
+            var currentValue = _boostEl.innerText / (100 / 255);
+            var nextValue = boostData.values[item][i];
 
             // We only care about boost values going down.
             if (nextValue < currentValue) {
@@ -112,125 +112,112 @@ function positionReplayObjects () {
               //
               // Take the diff and check how many frames before we should start to move the gauge.
 
-              const frameDiffRequired = (currentValue - nextValue) / (255 / 74)
+              var frameDiffRequired = (currentValue - nextValue) / (255 / 74);
 
               if (frameDiff > frameDiffRequired) {
-                continue
+                continue;
               }
 
-              const rawValue = boostData.values[item][i] + (frameDiff * (255/74))
+              var rawValue = boostData.values[item][i] + frameDiff * (255 / 74);
 
               if (rawValue < 0 || rawValue > 255) {
-                throw new Error("BoostRangeError")
+                throw new Error("BoostRangeError");
               }
 
-              const value = Math.ceil(rawValue * (100 / 255))
+              var _value = Math.ceil(rawValue * (100 / 255));
 
-              boostEl.style.backgroundSize = `${value}% 100%`;
-              boostEl.innerHTML = value
+              _boostEl.style.backgroundSize = _value + '% 100%';
+              _boostEl.innerHTML = _value;
 
-              if (value < 15) {
-                boostEl.style.color = '#000'
+              if (_value < 15) {
+                _boostEl.style.color = '#000';
               } else {
-                boostEl.style.color = ''
+                _boostEl.style.color = '';
               }
 
-              break
+              break;
             }
           }
         }
       }
     }
-  })
+  });
 
   // Figure out what the score is.
-  let team_0_score = 0
-  let team_1_score = 0
+  var team_0_score = 0;
+  var team_1_score = 0;
 
   goalData.forEach(function (item) {
     if (currentFrame >= item.frame) {
       if (item.PlayerTeam === 0) {
-        team_0_score++
+        team_0_score++;
       } else if (item.PlayerTeam === 1) {
-        team_1_score++
+        team_1_score++;
       }
     }
-  })
+  });
 
-  document.querySelector('.sim-Timer_Score-0').innerHTML = team_0_score
-  document.querySelector('.sim-Timer_Score-1').innerHTML = team_1_score
+  document.querySelector('.sim-Timer_Score-0').innerHTML = team_0_score;
+  document.querySelector('.sim-Timer_Score-1').innerHTML = team_1_score;
 
-  document.querySelector('.sim-Timeline_Inner').style.width = `${currentFrame / maxFrame * 100}%`;
+  document.querySelector('.sim-Timeline_Inner').style.width = currentFrame / maxFrame * 100 + '%';
 
   // Do any actors get removed in this frame?
   Object.keys(actorData).forEach(function (item) {
     if (actorData[item].left <= currentFrame) {
-      const objectName = `car-${item}`;
-      const carObject = scene.getObjectByName(objectName)
+      var objectName = 'car-' + item;
+      var carObject = scene.getObjectByName(objectName);
 
       if (carObject !== undefined) {
-        scene.remove(carObject)
+        scene.remove(carObject);
       }
 
-      const boostEl = document.querySelector(`.sim-Boost_Outer-actor${item}`)
+      var boostEl = document.querySelector('.sim-Boost_Outer-actor' + item);
 
       if (boostEl) {
-        boostEl.remove()
+        boostEl.remove();
       }
     }
-  })
+  });
 
   frameData[currentFrame].actors.forEach(function (actor, index) {
     // Does this car already exist in the scene.
-    const objectName = `car-${actor.id}`;
-    const carObject = scene.getObjectByName(objectName)
+    var objectName = 'car-' + actor.id;
+    var carObject = scene.getObjectByName(objectName);
 
     if (carObject === undefined) {
       // Add the car.
       if (carsLoading.indexOf(objectName) === -1) {
-        carsLoading.push(objectName)
+        carsLoading.push(objectName);
 
-        console.log(`[${objectName}] Calling addCar`)
+        console.log('[' + objectName + '] Calling addCar');
         if (actor.type === 'player') {
-          addCar(objectName, actor)
+          addCar(objectName, actor);
         } else if (actor.type === 'ball') {
-          addBall(objectName, actor)
+          addBall(objectName, actor);
         }
       }
     } else {
       // Reposition the car based on the latest data.
       if (actor.z < 0) {
-        console.error('Z value below 0 at frame', currentFrame)
+        console.error('Z value below 0 at frame', currentFrame);
       }
 
-      carObject.position.set(
-        actor.x * -1,
-        actor.y,
-        1
-        )
+      carObject.position.set(actor.x * -1, actor.y, 1);
 
       // If actor.z is 18, we want scaleFactor to be 1
       // if actor.z is 2048 we want scaleFactor to be 2
       // const scaleFactor = 4 - (((2048 - (actor.z - 18)) / 2048) / 4)
 
-      const scaleFactor = (
-          1 +  // Base scale, required to stop objects disappearing (by being scaled to 0 at ground level)
-          (2.75 * // Max scale factor.
-            (actor.z - 18) / 2048) // Currenct z value as a percentage of the arena height.
-          )
+      var scaleFactor = 1 + // Base scale, required to stop objects disappearing (by being scaled to 0 at ground level)
+      2.75 * ( // Max scale factor.
+      actor.z - 18) / 2048 // Currenct z value as a percentage of the arena height.
+      ;
 
-      carObject.scale.set(
-        scaleFactor,
-        scaleFactor,
-        scaleFactor
-        )
+      carObject.scale.set(scaleFactor, scaleFactor, scaleFactor);
 
       // Looks close.
-      carObject.rotation.set(
-        0,
-        0,
-        r(90) + actor.pitch * Math.PI * -1
-        )
+      carObject.rotation.set(0, 0, r(90) + actor.pitch * Math.PI * -1);
     }
-  })
+  });
 }
