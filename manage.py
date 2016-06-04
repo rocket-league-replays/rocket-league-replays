68138260999e16b332139a4576d4483406bfac89
Daniel Samuels <daniel@rocketleaguereplays.com>
2016-06-04 12:49:02 +0100

    Update manage.py to pull down the latest version of octane when using runserver.

--- manage.py
+++ manage.py
@@ -2,9 +2,45 @@
 import os
 import sys
 
+import requests
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rocket_league.settings.local")
 
     from django.core.management import execute_from_command_line
+
+    # Ensure Octane is kept up-to-date.
+    if 'runserver' in sys.argv[1]:
+        octane_release = requests.get('https://api.github.com/repos/tfausak/octane/releases/latest', headers={
+            'Authorization': 'Token {}'.format(os.getenv('GITHUB_TOKEN', ''))
+        }).json()
+
+        # What version do we have locally?
+        try:
+            os.remove('octane-binaries/.DS_Store')
+        except FileNotFoundError:
+            pass
+
+        current_binaries = os.listdir('octane-binaries')
+
+        current_version = '0.0.0'
+
+        if len(current_binaries) > 0:
+            current_version = current_binaries[0].split('-')[1]
+
+        if tuple(octane_release['name'].split('.')) > tuple(current_version.split('.')):
+            # Download the latest version.
+            for file in current_binaries:
+                os.remove('octane-binaries/{}'.format(file))
+
+            for asset in octane_release['assets']:
+                if 'windows' in asset['browser_download_url']:
+                    continue
+
+                os.system('wget -qP octane-binaries/ {} && gunzip -f octane-binaries/{} && chmod +x octane-binaries/{}'.format(
+                    asset['browser_download_url'],
+                    asset['name'],
+                    asset['name'].replace('.gz', ''),
+                ))
 
     execute_from_command_line(sys.argv)
