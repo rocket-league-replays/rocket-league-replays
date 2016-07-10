12cfd09a391ccdc8c10d91415de69031146118c6
Daniel Samuels <daniel@rocketleaguereplays.com>
2016-07-10 17:31:04 +0100

    Minor antenna replace change.

--- rocket_league/apps/replays/models.py
+++ rocket_league/apps/replays/models.py
@@ -796,7 +796,7 @@ class Player(models.Model):
                 'Decal': {'type': 'decal', 'replace': 'Skin_'},
                 'Wheels': {'type': 'wheels', 'replace': 'WHEEL_'},
                 'RocketTrail': {'type': 'trail', 'replace': 'Boost_'},
-                'Antenna': {'type': 'antenna', 'replace': 'Antenna_'},
+                'Antenna': {'type': 'antenna', 'replace': 'Antenna '},
                 'Topper': {'type': 'topper', 'replace': 'Hat_'},
             }
 
