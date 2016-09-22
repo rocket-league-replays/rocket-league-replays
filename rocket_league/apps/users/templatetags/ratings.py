29d9d8337e290faaf61dbdcaeb91d436cad8fad7
Daniel Samuels <daniel@rocketleaguereplays.com>
2016-09-23 00:04:07 +0100

    Simplify the header ratings code.

--- rocket_league/apps/users/templatetags/ratings.py
+++ rocket_league/apps/users/templatetags/ratings.py
@@ -1,52 +1,18 @@
 from django import template
 from django.conf import settings
 
-from ...replays.models import get_default_season
-from ..models import Profile, LeagueRating
-
+from ...replays.models import PLATFORM_STEAM, get_default_season
+from ..models import LeagueRating
 
 register = template.Library()
 
 
 @register.assignment_tag(takes_context=True)
-def league_data(context):
-    # Does this user have their Steam account connected?
-    user = context.request.user
-
-    if not user.is_authenticated():
-        return
-
-    try:
-        assert user.profile
-    except Exception as e:
-        print(e.__class__.__name__)
-
-        if e.__class__.__name__ == 'RelatedObjectDoesNotExist':
-            Profile.objects.create(
-                user=user
-            )
-
-    return user.profile.latest_ratings()
-
-
-@register.assignment_tag(takes_context=True)
 def latest_ratings(context):
-    ratings = LeagueRating.objects.filter(
-        steamid=context['steam_id'],
-        season_id=get_default_season(),
-    )[:1]
-
-    if ratings.count() > 0:
-        return {
-            settings.PLAYLISTS['RankedDuels']: ratings[0].duels,
-            '{}_division'.format(settings.PLAYLISTS['RankedDuels']): ratings[0].duels_division,
-            settings.PLAYLISTS['RankedDoubles']: ratings[0].doubles,
-            '{}_division'.format(settings.PLAYLISTS['RankedDoubles']): ratings[0].doubles_division,
-            settings.PLAYLISTS['RankedSoloStandard']: ratings[0].solo_standard,
-            '{}_division'.format(settings.PLAYLISTS['RankedSoloStandard']): ratings[0].solo_standard_division,
-            settings.PLAYLISTS['RankedStandard']: ratings[0].standard,
-            '{}_division'.format(settings.PLAYLISTS['RankedStandard']): ratings[0].standard_division,
-        }
+    return LeagueRating.objects.filter(
+        platform=PLATFORM_STEAM,
+        online_id=context['steam_id'],
+    )
 
 
 @register.assignment_tag
@@ -55,6 +21,15 @@ def get_ratings(uid):
         steamid=uid,
         season_id=get_default_season(),
     )[:50]
+
+
+@register.simple_tag
+def playlist_name(playlist, remove_prefix=False):
+    if playlist in settings.HUMAN_PLAYLISTS:
+        if remove_prefix:
+            return settings.HUMAN_PLAYLISTS[playlist].replace('Ranked ', '')
+        return settings.HUMAN_PLAYLISTS[playlist]
+    return playlist
 
 
 @register.simple_tag
