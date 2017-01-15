f1dfe0054d3047eff4497f10a228e728280b7550
Daniel Samuels <daniel@rocketleaguereplays.com>
2017-01-15 12:05:12 +0000

    Add patreon token renewal script.

---
+++ rocket_league/apps/site/management/commands/update_patreon_tokens.py
@@ -0,0 +1,27 @@
+import requests
+import os
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        r = requests.post('http://api.patreon.com/oauth2/token', data={
+            'grant_type': 'refresh_token',
+            'refresh_token': os.getenv('PATREON_REFRESH_TOKEN'),
+            'client_id': os.getenv('PATREON_CLIENT_ID'),
+            'client_secret': os.getenv('PATREON_CLIENT_SECRET'),
+        })
+
+        tokens = r.json()
+
+        with open('rocket_league/settings/secrets.py', 'r') as f:
+            settings_data = f.read()
+
+        settings_data = settings_data.replace(os.getenv('PATREON_REFRESH_TOKEN'), tokens['refresh_token'])
+        settings_data = settings_data.replace(os.getenv('PATREON_ACCESS_TOKEN'), tokens['access_token'])
+
+        print(settings_data)
+
+        with open('rocket_league/settings/secrets.py', 'w') as f:
+            f.write(settings_data)
