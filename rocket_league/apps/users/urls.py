b7b58f790fecf94f24a839bb7ca732d248f33dfd
Daniel Samuels <daniel@rocketleaguereplays.com>
2017-02-22 21:42:40 +0000

    Mark patreon settings URL redirect as temp.

--- rocket_league/apps/users/urls.py
+++ rocket_league/apps/users/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns(
     url(r'^user/(?P<username>[^/]+)/$', views.PublicProfileView.as_view(), name='profile'),
     url(r'^steam/(?P<steam_id>[^/]+)/$', views.SteamView.as_view(), name='steam'),
     url(r'^settings/$', views.SettingsView.as_view(), name='settings'),
-    url(r'^settings/patreon/$', RedirectView.as_view(pattern_name='users:settings'), name='patreon'),
+    url(r'^settings/patreon/$', RedirectView.as_view(pattern_name='users:settings', permanent=False), name='patreon'),
     url(r'^profile/settings/$', views.UserSettingsView.as_view(), name='password'),
     url(r'^user/stream/settings/$', views.StreamSettingsView.as_view(), name='stream_settings'),
     url(r'^user/stream/(?P<user_id>\d+)/(?P<method>\bbasic\b)/$', views.StreamDataView.as_view(), name='stream'),
