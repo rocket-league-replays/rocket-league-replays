787dce1218c98f2c574f120abc00ca6354efd538
Daniel Samuels <daniel.samuels1@gmail.com>
2018-05-21 21:19:02 +0100

    Handle stats values coming back as a dict

--- rocket_league/apps/users/management/commands/get_league_ratings.py
+++ rocket_league/apps/users/management/commands/get_league_ratings.py
@@ -177,6 +177,9 @@ class Command(BaseCommand):
                     # While we're here, get their stats as well.
                     stats = rl.get_stats_values_for_user(PLATFORMS_MAPPINGS[platform], online_ids)
 
+                    if isinstance(stats, dict):
+                        stats = [stats]
+
                     for player in players:
                         if platform == PLATFORM_STEAM:
                             online_id = player['user_id']
